8f7f9ded1787e0b856217505a0b690f7864ac756
Ben Cates <ben.cates@gmail.com>
2013-06-04 00:04:47 -0600

    Added skycons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,5 +11,36 @@ angular.module('daysOfSun')
     $http.jsonp("http://api.wunderground.com/api/" + WU_KEY + "/conditions/q/NM/Albuquerque.json?callback=loadWUData");
     $window.loadWUData = function (data) {
       $scope.currentConditions = data.current_observation.weather;
+      $scope.conditionIcon = (function (icon) {
+        switch (icon) {
+          case 'clear':
+          case 'sunny':
+          case 'mostlysunny':
+            return 'CLEAR_DAY';
+          case 'partlysunny':
+          case 'partlycloudy':
+            return 'PARTLY_CLOUDY_DAY';
+          case 'mostlycloudy':
+          case 'cloudy':
+            return 'CLOUDY';
+          case 'chancerain':
+          case 'chancetstorms':
+          case 'rain':
+          case 'tstorms':
+          case 'unknown':
+            return 'RAIN';
+          case 'chancesleet':
+          case 'sleet':
+            return 'SLEET';
+          case 'chanceflurries':
+          case 'chancesnow':
+          case 'flurries':
+          case 'snow':
+            return 'SNOW';
+          case 'fog':
+          case 'hazy':
+            return 'FOG';
+        }
+      })(data.current_observation.icon);
     };
   });
